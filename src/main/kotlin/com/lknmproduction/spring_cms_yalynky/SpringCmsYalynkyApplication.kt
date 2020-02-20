cab3dd71d05c02b2b710ac54592d4239a82eb062
kyrylo_kundik <goliathusua@gmail.com>
2020-02-20 16:20:10 +0200

    Initial commit

---
+++ src/main/kotlin/com/lknmproduction/spring_cms_yalynky/SpringCmsYalynkyApplication.kt
@@ -0,0 +1,11 @@
+package com.lknmproduction.spring_cms_yalynky
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCmsYalynkyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCmsYalynkyApplication>(*args)
+}
---
+++ src/test/kotlin/com/lknmproduction/spring_cms_yalynky/SpringCmsYalynkyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lknmproduction.spring_cms_yalynky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCmsYalynkyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
